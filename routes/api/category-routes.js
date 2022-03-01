df9cc89330f5db1721614d964176f79ee14276f3
Adolfo Gonzalez <agonzal177@gmail.com>
2022-03-01 00:31:29 -0500

    fixed product tag/ working in category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,12 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  res.send(await Category.findAll({
+    include: [{model: Product,}]
+  }))
 });
 
 router.get('/:id', (req, res) => {
